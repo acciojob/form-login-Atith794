386e095437a93dbaca7590a4e2d980970eeea41c
Atith794 <68183452+Atith794@users.noreply.github.com>
2023-10-11 07:06:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
-
+	const form12 = document.getElementById("form1");
+	const fn = form12.fname.value;
+	const ln = form12.lname.value;
+	alert(fn+" "+ln);
 }
+
+getFormvalue();
